f1e7a8966f7dbba1e6f0b45b2d9e0a63f5ad75b8
ozzyman <ajkerr@mail.com>
2019-07-21 12:30:47 +1000

    Add files via upload

--- BlueButter/BlueButter/main.c
+++ BlueButter/BlueButter/main.c
@@ -1,23 +1,26 @@
 #include <atmel_start.h>
-#include <util/delay.h>
 #include <usart_basic.h>
+#include <adc_basic.h>
+#include <util/delay.h>
+#include <string.h>
+#include <stdio.h>
 
 int main(void)
 {
+uint8_t character;
+
 	/* Initializes MCU, drivers and middleware */
 	atmel_start_init();
+	USART_0_init();
+	USART_0_enable();
 
 	/* Replace with your application code */
 	while (1) {
-		LED_set_level(true);
-		_delay_ms(100);
+		character = USART_0_read(); /* get a character from the UART Rx buffer */
+		USART_0_write(character);   /* and write it to the Tx buffer - i.e loopback */
+		LED_set_level(true);		/* flash the LED */
+		_delay_ms(1);
 		LED_set_level(false);
-		_delay_ms(100);
-		LED_set_level(true);
-		_delay_ms(100);
-		LED_set_level(false);
-		_delay_ms(1000);
-		USART_0_init(115200);
-			
+		_delay_ms(1);
 	}
 }
